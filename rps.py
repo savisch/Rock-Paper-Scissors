a8ebf305bbf37acb86ddc0bf0b6500e01cd7691e
Scotty G <savisch@gmail.com>
2020-11-24 15:43:23 -0600

    Pep8 Tweaks

--- rps.py
+++ rps.py
@@ -1,26 +1,30 @@
-#!/usr/bin/env python3
+
 import random
 import time
-"""This program plays a game of Rock, Paper, Scissors between two Players,
-and reports both Player's scores each round."""
+
+# This program plays a game of Rock, Paper, Scissors between two Players,
+# and reports both Player's scores each round
+
+
 moves = ['rock', 'paper', 'scissors']
 cycleMoves = ['rock', 'paper', 'scissors']
 count = 0
 player1_score = 0
 player2_score = 0
 
-"""The Player class is the parent class for all of the Players
-in this game"""
+
 def pause():
     time.sleep(1)
+
 
 class Player:
     def move(self):
         return 'rock'
-        
+
     def learn(self, my_move, their_move):
         self.their_move = their_move
         self.remember = self.their_move
+
 
 # a subclass called RandomPlayer that chooses moves
 # at Random from 'moves'([rock, paper, scissors])
@@ -28,23 +32,26 @@ class RandomPlayer(Player):
     def move(self):
         return random.choice(moves)
 
-# a subclass for a human player that takes input from a 
+
+# a subclass for a human player that takes input from a
 # human and validates input as 'rock', 'paper', 'scissors
 class HumanPlayer(Player):
-    name_response = input("What is your name?\n") 
+    name_response = input("What is your name?\n")
 
     def learn_name(self):
         self.name = self.name_response
         return self.name
 
     def move(self):
-        self.move_response = input("Enter 'rock', 'paper' or 'scissors': ").lower()
+        self.move_response = input("Enter 'rock', 'paper' or 'scissors': ")\
+            .lower()
         pause()
 
         if self.move_response in moves:
-             return self.move_response
+            return self.move_response
         else:
             self.move()
+
 
 # a ReflectPlayer subclass that remembers what move the opponent
 #  player made in the last round and plays that move this round
@@ -53,23 +60,25 @@ class ReflectPlayer(Player):
         global count
         if count == 0:
             play = random.choice(moves)
-            return play  
-        else: 
+            return play
+        else:
             return self.remember
 
-# a CyclePlayer subclass that remembers the move it made in the 
+
+# a CyclePlayer subclass that remembers the move it made in the
 # last rounds and cycles through the moves
 class CyclePlayer(Player):
     def move(self):
         play = cycleMoves.pop(cycleMoves.index(random.choice(cycleMoves)))
         return play
 
+
 def beats(one, two):
     if one != two:
         return ((one == 'rock' and two == 'scissors') or
                 (one == 'scissors' and two == 'paper') or
                 (one == 'paper' and two == 'rock'))
-        
+
 
 class Game:
     def __init__(self, p1, p2):
@@ -78,7 +87,8 @@ class Game:
 
     def rounds_query(self):
         try:
-            self.round_response = int(input("How many rounds would you like to play?\n"))
+            self.round_response = int(input("How many rounds would you like to \
+                play?\n"))
             return self.round_response
         except ValueError:
             self.rounds_query()
@@ -142,7 +152,7 @@ class Game:
         self.rounds_query()
         print("Let's start the Game!\n")
         pause()
-    
+
         for round in range(int(self.learn_rounds())):
             print(f"Round {round}:")
             pause()
@@ -151,10 +161,10 @@ class Game:
         print("Game over!")
         pause()
 
+
 if __name__ == '__main__':
     # game play will utilize a HumanPlayer for input and randomly
     # pick an opponent from the list of opponents
     opponents = [RandomPlayer(), ReflectPlayer(), CyclePlayer(), Player()]
     game = Game(HumanPlayer(), random.choice(opponents))
-    #game = Game(HumanPlayer(), Player())
     game.play_game()
